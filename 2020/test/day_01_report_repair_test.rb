c2b74dd0c85fd66002c02675ace0c62b47e9dbb2
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-12-10 20:09:04 +0100

    add test infrastructure and first dummy test

---
+++ 2020/test/day_01_report_repair_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class Day01Test < Minitest::Test
+  describe "Report repair" do
+    it "works" do
+      _(true).must_equal true
+    end
+  end
+end
---
+++ 2020/test/test_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.expand_path("..", __dir__)
+require "pry"
+
+require "minitest/spec"
+require "minitest/autorun"
