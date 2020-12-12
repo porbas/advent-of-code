f8105ad4444d42a7a9864a0fe117acfb435a967e
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-12-12 19:09:15 +0100

    refactor day 10 AdapterArray for part2

--- 2020/day_10_adapter_array.rb
+++ 2020/day_10_adapter_array.rb
@@ -1,26 +1,47 @@
 class AdapterArray
-  def initialize
-    @rating = 0
-    @diffs = {3 => 1}
+  def initialize(input)
+    @input = longest_chain input.sort
+    @diffs = @input
+      .each_cons(2)
+      .map {|(x,y)| y-x}
   end
 
-  def call(input)
-    @input = input.sort
-    @input.each {|joltage| plug_in joltage }
-    @diffs[1] * @diffs[3]
+  def diffs_product
+    counts = @diffs.tally
+    counts[1] * counts[3]
+  end
+
+  FACTORS = {
+    1 => 2,
+    2 => 4,
+    3 => 7,
+  }
+
+  def arrangements_count
+    @pull_outs = @diffs.each_cons(2)
+      .map {|fragment| fragment.inject(:+) < 3}
+    total = 1
+    consecutive_pulls = 0
+    @pull_outs.each do |pull|
+      if pull
+        consecutive_pulls += 1
+      else
+        total *= FACTORS[consecutive_pulls] if consecutive_pulls > 0
+        consecutive_pulls = 0
+      end
+    end
+    total
   end
 
   private
 
-  def plug_in(joltage)
-    diff = joltage - @rating
-    @diffs[diff] ||= 0
-    @diffs[diff] += 1
-    @rating = joltage
+  def longest_chain(joltages)
+    [0] + joltages + [joltages.last + 3]
   end
 end
 
 if (input_file = ARGV[0]) =~ /txt$/
   entries = File.readlines(input_file).map {|line| line.to_i}
-  puts AdapterArray.new.call(entries)
+  puts AdapterArray.new(entries).diffs_product
+  puts AdapterArray.new(entries).arrangements_count
 end
--- 2020/test/day_10_adapter_array_test.rb
+++ 2020/test/day_10_adapter_array_test.rb
@@ -6,9 +6,15 @@ class Day10Test < Minitest::Test
   describe AdapterArray do
     let(:short_example) { %w(16 10 15 5 1 11 7 19 6 12 4).map(&:to_i) }
     let(:larger_example) { %w(28 33 18 42 31 14 46 20 48 47 24 23 49 45 19 38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3).map(&:to_i) }
-    it "gives proper output" do
-      _(AdapterArray.new.call(short_example)).must_equal 35
-      _(AdapterArray.new.call(larger_example)).must_equal 220
+
+    it "gives proper diffs product" do
+      _(AdapterArray.new(short_example).diffs_product).must_equal 35
+      _(AdapterArray.new(larger_example).diffs_product).must_equal 220
+    end
+
+    it "gives proper arrangements count" do
+      _(AdapterArray.new(short_example).arrangements_count).must_equal 8
+      _(AdapterArray.new(larger_example).arrangements_count).must_equal 19208
     end
   end
 end
