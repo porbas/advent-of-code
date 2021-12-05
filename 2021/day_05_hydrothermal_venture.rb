b9b7c8fa2e2ae1c11367378976fda6360d58febf
Piotr Romańczuk <porbas@gmail.com>
2021-12-05 17:09:31 +0100

    day 05, part 1

---
+++ 2021/day_05_hydrothermal_venture.rb
@@ -0,0 +1,86 @@
+class DangerCalculator
+  def call(input_lines)
+    @matrix = Matrix.new(
+      input_lines.map {|l| Line.new(l)}.select {|l| l.horizontal? || l.vertical?}
+    )
+    @matrix.score(2)
+  end
+end
+
+class Matrix
+  def initialize(lines)
+    @lines = lines
+    @matrix = []
+    lines.each {|l| add_to_matrix(l)}
+  end
+
+  def inspect
+    @matrix.map do |row|
+      row.map do |cell|
+        cell.nil? ? '.' : cell.to_s
+      end.join('')
+    end.join("\n")
+  end
+
+  def score(treshold)
+    counter = 0
+    @matrix.each do |row|
+      row.map do |cell|
+        counter +=1 if cell && cell >= treshold
+      end
+    end
+    counter
+  end
+
+  private
+  def add_to_matrix(l)
+    l.points.each do |p|
+      @matrix[p.x] ||= []
+      @matrix[p.x][p.y] ||= 0
+      @matrix[p.x][p.y] += 1
+    end
+  end
+end
+
+class Line
+  def initialize(description)
+    x1,y1,x2,y2 = description.split(/,|->/).map(&:to_i)
+    @p1 = Point.new(x1, y1)
+    @p2 = Point.new(x2, y2)
+    normalize
+  end
+
+  def vertical?
+    @p1.x == @p2.x
+  end
+  def horizontal?
+    @p1.y == @p2.y
+  end
+
+  def points
+    if vertical?
+      (@p1.y..@p2.y).map {|y| Point.new(@p1.x, y) }
+    elsif horizontal?
+      (@p1.x..@p2.x).map {|x| Point.new(x, @p1.y) }
+    else
+      []
+    end
+  end
+
+  private
+
+  def normalize
+    if @p1.x >= @p2.x && @p1.y >= @p2.y
+      @p1, @p2 = @p2, @p1
+    end
+  end
+end
+
+class Point < Struct.new(:x, :y)
+
+end
+
+if (input_file = ARGV[0]) =~ /txt$/
+  lines = File.readlines(input_file).map(&:strip)
+  puts DangerCalculator.new.call(lines)
+end
---
+++ 2021/test/day_05_hydrothermal_venture_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+require 'day_05_hydrothermal_venture'
+
+class Day05Test < Minitest::Test
+  EXAMPLE = <<~END.split("\n")
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+END
+
+  describe DangerCalculator do
+    it "returns number of danger areas" do
+      _(DangerCalculator.new.call(EXAMPLE)).must_equal(5)
+    end
+  end
+
+end
+
